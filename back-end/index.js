c1bde188bae064a0f2a32f8e2087d8e51b42595b
Alex Reyes-Paredes <areyespa@meta.com>
2025-06-18 10:55:37 -0700

    This commit is for server running command

--- back-end/index.js
+++ back-end/index.js
@@ -5,3 +5,7 @@ const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my KudoBoard App!')
+})
