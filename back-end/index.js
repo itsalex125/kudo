ab14cf76b4517a37e64a68b2f2697381a8bf56e6
Alex Reyes-Paredes <areyespa@meta.com>
2025-06-18 11:17:25 -0700

    This commit is to undone a change

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT
+const PORT = 9009
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
