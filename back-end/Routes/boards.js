85f828cd96b90a7a0c36e7f663e3c22685ccf62d
Alex Reyes-Paredes <areyespa@meta.com>
2025-06-18 10:16:23 -0700

    This commit is to attempt to deploy full stack website

---
+++ back-end/Routes/boards.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const { PrismaClient } = require('@prisma/client')
+const router = express.Router()
+const prisma = new PrismaClient();
+
+router.get("/", async(req,res)=> {
+    const boards = await prisma.board.findMany();
+    console.log(boards);
+    res.json(boards);
+})
+
+module.exports = router 
