95ce72af8c44d60638a93576e932a3306128c395
Alex Reyes-Paredes <areyespa@meta.com>
2025-06-19 14:36:24 -0700

    This commit is to push the backend code for my project

--- back-end/prisma/migrations/20250618000720_init_kudo_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Card" (
-    "boardId" SERIAL NOT NULL,
-    "message" TEXT NOT NULL,
-    "gif" TEXT NOT NULL,
-    "delete" BOOLEAN NOT NULL,
-
-    CONSTRAINT "Card_pkey" PRIMARY KEY ("boardId")
-);
